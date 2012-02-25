5a381eb707ac8ffe2e0f6e91eef36d11bb484a19
Yoshiki <yoshiki89@gmail.com>
2012-02-25 13:46:58 -0700

    Changed the class a little to make it easier to understand

---
+++ Demo/Demo/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  Demo
+//
+//  Created by Yoshiki - Vázquez Baeza on 16/02/12.
+//  Copyright (c) 2012 Polar Bears Nanotechnology Research ©. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
